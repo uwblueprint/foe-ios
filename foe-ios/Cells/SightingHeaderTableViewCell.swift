60061aecf0e6ae5d57a11c3b6e2018f5522a05b8
John Salaveria <jj.salaveria@gmail.com>
2018-05-30 18:08:52 -0400

    organizing swift files into folders

---
+++ foe-ios/Cells/SightingHeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SightingHeaderTableViewCell.swift
+//  foe-ios
+//
+//  Created by John Salaveria on 2018-05-28.
+//  Copyright © 2018 Blueprint. All rights reserved.
+//
+
+import UIKit
+
+class SightingHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
