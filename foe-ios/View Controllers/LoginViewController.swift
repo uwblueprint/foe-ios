82bc7852ab31660befcb583c8a615e9c22c69f80
John Salaveria <jj.salaveria@gmail.com>
2018-06-13 21:25:02 -0400

    UI styling for login

--- foe-ios/View Controllers/LoginViewController.swift
+++ foe-ios/View Controllers/LoginViewController.swift
@@ -10,20 +10,101 @@ import UIKit
 import Alamofire
 import SwiftKeychainWrapper
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    var frameView : UIView!
+    var activeTextField : UITextField?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.hideKeyboardWhenTappedAround()
         
+        for item in [emailTextField, passwordTextField] {
+            let textField = item as UITextField?
+            textField!.delegate = self
+        }
+        
+        renderUIButtons()
+        setupKeyboardNotificationCenter()
+        self.hideKeyboardWhenTappedAround()
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeTextField = textField
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        textField.layoutIfNeeded()
+        
+        if (activeTextField == emailTextField) {
+            passwordTextField.becomeFirstResponder()
+        }
+        
+        return false
+    }
+    
+    
+    func setupKeyboardNotificationCenter() {
+        let center: NotificationCenter = NotificationCenter.default
+        center.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        center.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardDidShow(notification:NSNotification) {
+        let info : NSDictionary = notification.userInfo! as NSDictionary
+        let keyboardSize = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        let keyboardY = self.view.bounds.height - keyboardSize.height
+        let padding: CGFloat = 100
+        let activeTextFieldY : CGFloat! = self.activeTextField?.frame.origin.y
+        
+        if ((self.view.frame.origin.y >= 0.0) && (activeTextFieldY > keyboardY - padding)) {
+            UIView.animate(withDuration: 0.25, delay: 0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                print("animate")
+                self.view.frame = CGRect(x: 0, y: self.view.frame.origin.y - (activeTextFieldY! - (keyboardY - padding)), width: self.view.bounds.width, height: self.view.bounds.height)
+            }, completion: nil)
+        }
+        else {
+            UIView.animate(withDuration: 0.25, delay: 0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+                self.view.frame.origin.y = 0;
+            }, completion: nil)
+        }
+    }
+    
+    func keyboardWillHide(notification:NSNotification) {
+        UIView.animate(withDuration: 0.25, delay: 0, options: UIViewAnimationOptions.curveEaseInOut, animations: {
+            self.view.frame.origin.y = 0;
+        }, completion: nil)
+    }
+    
+    // render button corners and shadow
+    func renderUIButtons() {
+        
+        for item in [signInButton, facebookButton] {
+            let button = item as UIButton?
+            button!.layer.cornerRadius = button!.frame.height/2
+            
+            let shadowPath = UIBezierPath(rect: button!.bounds)
+            button!.layer.masksToBounds = false
+            button!.layer.shadowColor = UIColor.black.cgColor
+            button!.layer.shadowOffset = CGSize(width: 0.0, height: 6.0)
+            button!.layer.shadowOpacity = 0.16
+            button!.layer.shadowPath = shadowPath.cgPath
+            button!.layer.shadowRadius = 6
+            
+        }
         
     }
-
     // TODO: validation of inputs: email has domain, password must be >= 8 chars
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    @IBOutlet weak var emailPrompt: UILabel!
+    @IBOutlet weak var emailOutline: UIView!
+    @IBOutlet weak var passwordPrompt: UILabel!
+    @IBOutlet weak var passwordOutline: UIView!
+    
     @IBAction func loginButtonClicked(_ sender: Any) {
         postLogin()
     }
@@ -51,6 +132,36 @@ class LoginViewController: UIViewController {
             }
         }
     }
+    
+    func renderInputActiveState(label: UILabel, outline: UIView, isActive: Bool) {
+        let color : UIColor = isActive ? UIColor(red:0.12, green:0.75, blue:0.39, alpha:1.0) : UIColor(red:0.88, green:0.88, blue:0.88, alpha:1.0)
+        
+        UIView.transition(with: label, duration: 0.2, options: .transitionCrossDissolve, animations: {
+            label.textColor = color
+        }, completion: nil)
+        
+        UIView.transition(with: outline, duration: 0.2, options: .transitionCrossDissolve, animations: {
+            outline.backgroundColor = color
+        }, completion: nil)
+    }
+    
+    @IBAction func emailEditingDidBegin(_ sender: Any) {
+        renderInputActiveState(label: emailPrompt, outline: emailOutline, isActive: true)
+    }
+    
+    @IBAction func emailEditingDidEnd(_ sender: Any) {
+        renderInputActiveState(label: emailPrompt, outline: emailOutline, isActive: false)
+    }
+    
+    @IBAction func passwordEditingDidBegin(_ sender: Any) {
+        renderInputActiveState(label: passwordPrompt, outline: passwordOutline, isActive: true)
+    }
+    
+    @IBAction func passwordEditingDidEnd(_ sender: Any) {
+        renderInputActiveState(label: passwordPrompt, outline: passwordOutline, isActive: false)
+    }
+    
+    
     
     private func postLogin() {
         let parameters: Parameters = [
