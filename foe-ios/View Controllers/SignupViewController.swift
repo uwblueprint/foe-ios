f893b41ce2daef505504fc221b26fc624a2b9614
Dinah Shi <dinahshi28@gmail.com>
2018-06-19 09:33:51 -0400

    Remove unused code

--- foe-ios/View Controllers/SignupViewController.swift
+++ foe-ios/View Controllers/SignupViewController.swift
@@ -61,15 +61,4 @@ class SignupViewController: UIViewController {
     @IBAction func closeButtonTouchedUpInside(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
