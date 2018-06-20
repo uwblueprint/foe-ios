3945aa131c045bb84578674a08ce45bda0e27eea
Dinah Shi <dinahshi28@gmail.com>
2018-06-20 17:02:14 -0400

    Remove references to facebook login

--- foe-ios/AppDelegate.swift
+++ foe-ios/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import FBSDKCoreKit
 import GooglePlaces
 
 @UIApplicationMain
@@ -16,20 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
         
         GMSPlacesClient.provideAPIKey(GoogleAPIKey)
         
         UIApplication.shared.statusBarStyle = .default
         
         return true
-    }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        
-        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
-        
-        return handled
     }
     
     func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
